a33d1069b8a05959b34c6074046fdc817382d13c
izabelka <i.konstanciak@gmail.com>
2024-01-02 18:08:03 -0800

    Add customers page - to do: Suspense, create customer

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,25 @@
+import Table from '@/app/ui/customers/table';
+// import { CustomersTableSkeleton } from '@/app/ui/skeletons';
+// import { Suspense } from 'react';
 import { Metadata } from 'next';
  
 export const metadata: Metadata = {
   title: 'Customers',
 };
+ 
+export default async function Page({
+  searchParams, 
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  }
+}) {
+  const query = searchParams?.query || '';
 
-export default function Page() {
-  return <p>Customers Page</p>;
+  return (
+    <div className="w-full">
+      <Table query={query} />
+    </div>
+  );
 }
